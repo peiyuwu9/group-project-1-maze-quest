5e75530765d34f92538888ed0b8e0d8d9807d828
Alexis Attardo <AlexisAtt@users.noreply.github.com>
2019-10-15 23:24:10 -0400

    treasure message added

--- assets/javascript/maze.js
+++ assets/javascript/maze.js
@@ -421,12 +421,12 @@ var firebaseConfig = {
 
         //When players' location match exit location, players win.
         if (maze[player.loc].state == 2) {
-            document.getElementById("moves").innerText = "You win!";
+            document.getElementById("moves").innerText = "You win the treasure!";
             // location.reload();
         }
 
         if (maze[player2.loc2].state == 2) {
-            document.getElementById("moves2").innerText = "You win!";
+            document.getElementById("moves2").innerText = "You win the treasure!";
             // location.reload();
         }
     
